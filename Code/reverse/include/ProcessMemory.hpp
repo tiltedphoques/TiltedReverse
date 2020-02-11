f33b8159ef5607e34628658cd7a0d512438f8215
Force67 <prelink835@gmail.com>
2020-02-11 20:02:24 +0100

    Update ProcessMemory.hpp

--- Code/reverse/include/ProcessMemory.hpp
+++ Code/reverse/include/ProcessMemory.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <AutoPtr.hpp>
 #include <type_traits>
 
 namespace TiltedPhoques
@@ -23,4 +24,20 @@ namespace TiltedPhoques
         void* m_pMemoryLocation;
         size_t m_size;
     };
+
+    template<typename TVar, typename TAddr>
+    inline bool Write(const TAddr addr, const TVar val, const size_t offset = 0) {
+        AutoPtr<TVar> ptr((uintptr_t)(addr));
+
+        ProcessMemory mem(ptr.GetPtr(), sizeof(TVar));
+        return mem.Write(val, offset);
+    }
+
+    template<typename TAddr>
+    inline bool Nop(const TAddr addr, size_t len) {
+        AutoPtr<TAddr> ptr((uintptr_t)addr);
+
+        ProcessMemory mem(ptr.GetPtr(), len);
+        return mem.WriteBuffer(reinterpret_cast<const unsigned char*>(ptr.GetPtr()), len, 0);
+    }
 }
