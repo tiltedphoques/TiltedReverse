43346faf0a0ba9534b9c5b3da71c78c9265b8e9e
Maxime Griot <yamashi@live.com>
2020-02-11 21:21:16 +0100

    Coding style changes

--- Code/reverse/include/ProcessMemory.hpp
+++ Code/reverse/include/ProcessMemory.hpp
@@ -10,6 +10,8 @@ namespace TiltedPhoques
         ProcessMemory(void* apMemoryLocation, size_t aSize) noexcept;
         ~ProcessMemory();
 
+        TP_NOCOPYMOVE(ProcessMemory);
+
         template<class T, typename = std::enable_if_t<std::is_pod<T>::value>>
         bool Write(const T & acData, const size_t aOffset = 0) const noexcept
         {
@@ -25,19 +27,21 @@ namespace TiltedPhoques
         size_t m_size;
     };
 
-    template<typename TVar, typename TAddr>
-    inline bool Write(const TAddr addr, const TVar val, const size_t offset = 0) {
-        AutoPtr<TVar> ptr((uintptr_t)(addr));
+    template<typename TVar, typename TAddress>
+    bool Write(const TAddress acAddress, const TVar acVal, const size_t acOffset = 0)
+    {
+        AutoPtr<TVar> ptr(static_cast<uintptr_t>(acAddress));
 
-        ProcessMemory mem(ptr.GetPtr(), sizeof(TVar));
-        return mem.Write(val, offset);
+        ProcessMemory memory(ptr.GetPtr(), sizeof(TVar));
+        return memory.Write(acVal, acOffset);
     }
 
-    template<typename TAddr>
-    inline bool Nop(const TAddr addr, size_t len) {
-        AutoPtr<TAddr> ptr((uintptr_t)addr);
+    template<typename TAddress>
+    bool Nop(const TAddress acAddress, size_t aLength)
+    {
+        AutoPtr<TAddress> ptr(static_cast<uintptr_t>(acAddress));
 
-        ProcessMemory mem(ptr.GetPtr(), len);
-        return mem.WriteBuffer(reinterpret_cast<const unsigned char*>(ptr.GetPtr()), len, 0);
+        const ProcessMemory memory(ptr.GetPtr(), aLength);
+        return memory.WriteBuffer(reinterpret_cast<const unsigned char*>(ptr.GetPtr()), aLength, 0);
     }
 }
