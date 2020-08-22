e3cf8a658d05a67d4750bc5bf87fb95355154e8f
Ofek <ofeklavie@gmail.com>
2020-08-22 15:30:23 +0300

    Free mouse capture on exception

--- Code/reverse/src/Debug.cpp
+++ Code/reverse/src/Debug.cpp
@@ -1,10 +1,13 @@
 #include <Debug.hpp>
 #include <thread>
+#include <cassert>
 
 #include <windows.h>
 
 namespace TiltedPhoques
 {
+    PVOID vehHandler = NULL;
+
     void Debug::WaitForDebugger() noexcept
     {
         while (!IsDebuggerPresent())
@@ -17,9 +20,35 @@ namespace TiltedPhoques
     {
         if (AllocConsole())
         {
-            freopen("CONOUT$", "w", stdout);
+            FILE* file = freopen("CONOUT$", "w", stdout);
+            assert(file != nullptr);
             SetConsoleTitleA("Tilted Reverse Console");
             SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_RED);
+        }
+    }
+
+    LONG WINAPI OnException(PEXCEPTION_POINTERS ExceptionInfo) {
+        ReleaseCapture(); // Release Skyrim's mouse capture - best effort
+
+        // Perhaps EXCEPTION_EXECUTE_HANDLER is more in order, in case someone after us handles the exception graceully?
+        return EXCEPTION_CONTINUE_SEARCH;
+
+        UNREFERENCED_PARAMETER(ExceptionInfo);
+    }
+
+    void Debug::OnAttach() noexcept
+    {
+        const int CALL_LAST = 0;
+        const int CALL_FIRST = 1;
+        assert(vehHandler == NULL);
+        vehHandler = AddVectoredExceptionHandler(CALL_LAST, OnException);
+        assert(vehHandler != NULL);
+    }
+
+    void Debug::OnDettach() noexcept
+    {
+        if (vehHandler) {
+            RemoveVectoredExceptionHandler(vehHandler);
         }
     }
 }
