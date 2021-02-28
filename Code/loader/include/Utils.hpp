6ced2fdc399654da8f07e6f442100e7266f35cb3
Force67 <prelink835@gmail.com>
2021-02-28 20:01:53 +0100

    yeet old loader :monkey:

--- Code/loader/include/Utils.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-#include <string>
-
-namespace Utils
-{
-    std::wstring ErrorToString(uint32_t errorCode);
-}
--- xmake.lua
+++ xmake.lua
@@ -22,31 +22,6 @@ target("TiltedReverse")
     add_headerfiles("Code/reverse/include/*.hpp", {prefixdir = "TiltedReverse"})
     add_packages("tiltedcore", "mimalloc", "hopscotch-map", "minhook")
 
-target("Loader")
-    set_kind("binary")
-    set_group("Libraries")
-    add_includedirs(
-        "Code/reverse/include/",
-        "Code/loader/include/",
-        {public = true})
-    add_files("Code/loader/src/*.cpp")
-    add_headerfiles("Code/loader/include/*.hpp", {prefixdir = "Loader"})
-    add_packages("tiltedcore", "mimalloc", "hopscotch-map")
-    add_syslinks("user32", "shell32")
-
-target("Proxy")
-    set_kind("shared")
-    set_group("Libraries")
-    add_includedirs(
-        "Code/reverse/include/",
-        "Code/loader/include/",
-        {public = true})
-    add_files(
-        "Code/loader/src/Config.cpp",
-        "Code/proxy/src/*.cpp")
-    add_headerfiles("Code/loader/include/*.hpp", {prefixdir = "Loader"})
-    add_packages("tiltedcore", "mimalloc", "hopscotch-map")
-
 target("TiltedReverse_Tests")
     set_kind("binary")
     set_group("Tests")
